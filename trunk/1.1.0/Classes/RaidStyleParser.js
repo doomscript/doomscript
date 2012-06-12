54237a6d9f8f2f73e0e8e83cc38744b926007ee4
doomcat <doomcat@7df05b64-3613-446f-a92b-41441ce7a415>
2012-06-12 11:54:45 +0000

    Groundwork for raidstyle feature

---
+++ trunk/1.1.0/Classes/RaidStyleParser.js
@@ -0,0 +1,21 @@
+		/************************************/
+		/********** RaidStyle Class *********/
+		/************************************/
+		
+		// Class to parse raid style parameters into CSS stuff
+		window.RaidMenu = Class.create({
+			initialize: function(params)
+			{
+				// Variables
+				this.filter;
+				this.style;
+				
+				// Split the params at the + that divides the filter from the style
+				var parts = params.split("\+");
+				
+				var filterText = params[0].trim();
+				var styleText = params[1].trim();
+				
+				
+			}
+		});
