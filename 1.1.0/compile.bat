aee9b0cd69ecb18300875790e8317f6b201f53eb
Sven <doomcat@users.noreply.github.com>
2012-06-13 12:55:02 +0000

    fixed#31 Created similar .bat files for Windows users to match the existing compile and compileDebug shell scripts

---
+++ 1.1.0/compile.bat
@@ -0,0 +1,4 @@
+@echo OFF
+copy /b header.js+Classes\*.js+MenuTabs\*.js+ChatCommands\*.js+Utilities\*.js+footer.js 124753_nightly.user.js
+echo "124753_nightly.user.js created at %date% %time%";
+pause
---
+++ 1.1.0/compileDebug.bat
@@ -0,0 +1,4 @@
+@echo OFF
+copy /b header.js+Classes\*.js+MenuTabs\*.js+ChatCommands\*.js+Experimental\*.js+Utilities\*.js+footer.js 124753_nightly.user.js
+echo "124753_nightly.user.js created at %date% %time%";
+pause
