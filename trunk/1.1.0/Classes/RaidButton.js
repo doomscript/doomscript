1cee0e71a2a98f925e834c4dce0791aa1156e46b
doomcat <doomcat@7df05b64-3613-446f-a92b-41441ce7a415>
2012-11-30 21:34:56 +0000

    WR code part 2

--- trunk/1.1.0/Classes/RaidButton.js
+++ trunk/1.1.0/Classes/RaidButton.js
@@ -5,10 +5,11 @@
 		window.RaidButton = Class.create({
 			initialize: function(name, className, callback)
 			{
-				this.name = name;
+				this.name = name || "";
 				this.callback = callback;
-				this.node = new Element("li");
+				this.node = new Element("li", {"class": "DC_LoaTS_button_wrapper " + className + "Wrapper"});
 				this.anchor = new Element("a", {"class": "DC_LoaTS_button " + className});
+				this.anchor.appendChild(document.createTextNode(this.name));
 				this.anchor.observe("click", function(clickEvent)
 				{
 					this.execute(clickEvent);
