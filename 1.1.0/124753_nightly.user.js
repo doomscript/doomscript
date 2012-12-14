d3c2eb4b32141396b6b26c18f472c02f8c11bc5c
Sven <doomcat@users.noreply.github.com>
2012-12-14 21:54:08 +0000

    Bad time on WR

--- 1.1.0/124753_nightly.user.js
+++ 1.1.0/124753_nightly.user.js
@@ -8926,24 +8926,24 @@ DC_LoaTS_Helper.raids =
 		};
 	// World Raid Data, if there is any
 
-/*
+
 	DC_LoaTS_Helper.worldRaidInfo = {
 		name: "Raging Snowman",
 		
 		spawnType: "Rare Spawn",
 		
-		startDate: "12/10/2012",
-		timerEnds: "2012-12-11T19:15:28Z",
+		startDate: "12/14/2012",
+		timerEnds: "2012-12-15T13:15:28Z",
 		
-		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=5675829&kv_difficulty=1&kv_raid_boss=raging_snowman&kv_hash=yWu6Wi6TV8",
-		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10785-Rare-Spawn-12-10-12-Raging-Snowman!",
-		infoUrlTitle: "'Rare Spawn 12/10/12 - Raging Snowman!' Official Announcement",
+		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=5759546&kv_difficulty=1&kv_raid_boss=raging_snowman&kv_hash=vklLQtnhn7",
+		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10842-Raging-Snowman-12-14-12",
+		infoUrlTitle: "'Rare Spawn 12/14/12 - Raging Snowman!' Official Announcement",
 		
 		lootTableImageUrl: "http://i.imgur.com/57n52.jpg?1"
 		
 	};
 
-*/
+
 
 	// End World Raid Data
 	
