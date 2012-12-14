aea913fa8bbea45db2b36c04ad1473d9f02d166a
doomcat <doomcat@7df05b64-3613-446f-a92b-41441ce7a415>
2012-12-14 21:54:08 +0000

    Bad time on WR

--- trunk/1.1.0/Utilities/WorldRaidData.js
+++ trunk/1.1.0/Utilities/WorldRaidData.js
@@ -7,7 +7,7 @@
 		spawnType: "Rare Spawn",
 		
 		startDate: "12/14/2012",
-		timerEnds: "2012-15-11T13:15:28Z",
+		timerEnds: "2012-12-15T13:15:28Z",
 		
 		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=5759546&kv_difficulty=1&kv_raid_boss=raging_snowman&kv_hash=vklLQtnhn7",
 		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10842-Raging-Snowman-12-14-12",
