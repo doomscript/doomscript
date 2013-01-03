e19f53bf6890d13be0aef09d8dd2bdd9eb24376e
Dan Stace <Sycdan@users.noreply.github.com>
2013-01-03 20:54:28 +0000

    Changed WR name to Space Pox

--- 1.1.0/Utilities/WorldRaidData.js
+++ 1.1.0/Utilities/WorldRaidData.js
@@ -1,26 +1,19 @@
+		// World Raid Data, if there is any
 
-	// World Raid Data, if there is any
-
-
-	DC_LoaTS_Helper.worldRaidInfo = {
-		name: "Christmas Campaign",
+		DC_LoaTS_Helper.worldRaidInfo = {
+			name: "Space Pox",
+			
+			spawnType: "World Raid",
+			
+			startDate: "12/19/2012",
+			timerEnds: "2012-12-21T23:00:07Z",
+			
+			raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=5872044&kv_difficulty=1&kv_raid_boss=christmas_montage&kv_hash=5XzvkGP6E8",
+			infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10905-New-World-Raid-Christmas-Campaign!",
+			infoUrlTitle: "'World Raid: Space Pox #2!' Official Announcement",
+			
+			lootTableImageUrl: "http://i.imgur.com/A1kXq.jpg"
+			
+		};
 		
-		spawnType: "World Raid",
-		
-		startDate: "12/19/2012",
-		timerEnds: "2012-12-21T23:00:07Z",
-		
-		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=5872044&kv_difficulty=1&kv_raid_boss=christmas_montage&kv_hash=5XzvkGP6E8",
-		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10905-New-World-Raid-Christmas-Campaign!",
-		infoUrlTitle: "'New World Raid: Christmas Campaign!' Official Announcement",
-		
-		lootTableImageUrl: "http://i.imgur.com/A1kXq.jpg"
-		
-	};
-
-
-
-	// End World Raid Data
-	
-	
-	
+		// End World Raid Data
