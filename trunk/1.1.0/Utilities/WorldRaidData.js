64cd6fe4d90ebf5615615351fe070dd5bd58ffbb
Sycdan <Sycdan@7df05b64-3613-446f-a92b-41441ce7a415>
2013-01-24 18:58:03 +0000

    Added all info for Pox world raid, except exact time and raid URL (which instead will show approximately when it will be summoned)

--- trunk/1.1.0/Utilities/WorldRaidData.js
+++ trunk/1.1.0/Utilities/WorldRaidData.js
@@ -1,26 +1,23 @@
 
 	// World Raid Data, if there is any
-	// Note for space pox: Change the kv_raid_boss to wr_space_pox
+	
+	// DC_LoaTS_Helper.worldRaidInfo.timerEnds = "2013-01-23T23:00:00Z" // 6PM EST / 11PM GMT
 
-/*
 	DC_LoaTS_Helper.worldRaidInfo = {
 		name: "Intergalactic Space Pox",
 		
 		spawnType: "World Raid",
 		
-		startDate: "01/09/2013",
-		timerEnds: "1999-01-12T23:00:00Z",
+		startDate: "01/25/2013",
+		timerEnds: "2013-01-28T22:15:00Z",
 		
-		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=5872044&kv_difficulty=1&kv_raid_boss=christmas_montage&kv_hash=5XzvkGP6E8",
-		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10905-New-World-Raid-Christmas-Campaign!",
-		infoUrlTitle: "'New World Raid: Christmas Campaign!' Official Announcement",
-		
-		lootTableImageUrl: "http://i.imgur.com/A1kXq.jpg"
-		
+		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=6482771&kv_difficulty=1&kv_hash=eU5NgJRuPq&kv_raid_boss=Will_Be_Summoned_Around_10PM_GMT",
+		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?11432-Fight-the-flu-fight-Space-Pox!",
+		infoUrlTitle: "'Fight the flu, fight Space Pox!' Official Announcement",
+		lootTableImageUrl: "http://i.imgur.com/BD3yH.jpg"
 	};
-*/
 
-	// DC_LoaTS_Helper.worldRaidInfo.timerEnds="2013-01-18T23:20:18Z"
+/*
 	DC_LoaTS_Helper.worldRaidInfo = {
 			name: "Space Pox Mary",
 			
@@ -32,13 +29,8 @@
 			raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=6482771&kv_difficulty=1&kv_raid_boss=space_pox_mary&kv_hash=eU5NgJRuPq",
 			infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?11294-Last-Chance-Space-Pox-Mary",
 			infoUrlTitle: "Last Chance: Space Pox Mary - Official Announcement",
-
 			lootTableImageUrl: "http://i.imgur.com/TMgZx.jpg"
 		};
-
-
+*/
 
 	// End World Raid Data
-	
-	
-	
