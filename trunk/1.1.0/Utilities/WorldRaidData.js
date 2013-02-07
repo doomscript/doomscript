19d4090849dbb559fb220793e89d7ea01edfc51b
Sycdan <Sycdan@7df05b64-3613-446f-a92b-41441ce7a415>
2013-02-07 23:03:49 +0000

    Added Cerebral CEO RS

--- trunk/1.1.0/Utilities/WorldRaidData.js
+++ trunk/1.1.0/Utilities/WorldRaidData.js
@@ -3,6 +3,37 @@
 	
 	// DC_LoaTS_Helper.worldRaidInfo.timerEnds = "2013-01-28T22:20:19Z" // 6PM EST / 11PM GMT
 
+	DC_LoaTS_Helper.worldRaidInfo = {
+		name: "Cerebral CEO",
+		
+		spawnType: "Raid Spawn",
+		
+		startDate: "02/07/2013",
+		timerEnds: "2013-02-08T22:40:49Z",
+		
+		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_boss=cerebral_ceo&kv_difficulty=1&kv_raid_id=6894478&kv_hash=gkT87rGS1L",
+		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?11606-Corporate-Takedown-Fight-the-Cerebral-CEO!",
+		infoUrlTitle: "'Corporate Takedown - Fight the Cerebral CEO! ' Official Announcement",
+		lootTableImageUrl: "http://i.imgur.com/7TzuHjl.jpg"
+	};
+	
+	/*
+	DC_LoaTS_Helper.worldRaidInfo = {
+		name: "Cerebral Destroyer",
+		
+		spawnType: "World Raid",
+		
+		startDate: "01/25/2013",
+		timerEnds: "2013-01-28T22:20:19Z",
+		
+		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=6648986&kv_difficulty=1&kv_raid_boss=wr_space_pox&kv_hash=0P9ft37ffs",
+		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?10224-Delirium-of-the-Cerebral-Destroyer",
+		infoUrlTitle: "'Delirium of the Cerebral Destroyer' Official Announcement",
+		lootTableImageUrl: "http://i.imgur.com/XlWhw.jpg"
+	};
+	*/
+
+/*
 	// Ensure raid_boss=wr_space_pox in raidUrl
 	DC_LoaTS_Helper.worldRaidInfo = {
 		name: "Intergalactic Space Pox",
@@ -17,6 +48,7 @@
 		infoUrlTitle: "'Fight the flu, fight Space Pox!' Official Announcement",
 		lootTableImageUrl: "http://i.imgur.com/BD3yH.jpg"
 	};
+*/
 
 /*
 	DC_LoaTS_Helper.worldRaidInfo = {
