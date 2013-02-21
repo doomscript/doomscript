e45dc2e0c0758ee48a88ae1d38016e9d1cbd4361
Sven <doomcat@users.noreply.github.com>
2013-02-21 23:44:38 +0000

    CEO RS started

--- 1.1.0/124753_nightly.user.js
+++ 1.1.0/124753_nightly.user.js
@@ -9901,21 +9901,21 @@ DC_LoaTS_Helper.raids =
     // We can leave stuff commented out in here, but don't let it get too big. This gets downloaded with /urd
 	
 	// DC_LoaTS_Helper.worldRaidInfo.timerEnds = "2013-01-28T22:20:19Z" // 6PM EST / 11PM GMT
-/*
+
 	DC_LoaTS_Helper.worldRaidInfo = {
 		name: "Cerebral CEO",
 		
 		spawnType: "Rare Spawn",
 		
 		startDate: "02/21/2013",
-		timerEnds: "2013-02-22T22:30:00Z",
+		timerEnds: "2013-02-22T22:21:04Z",
 		
-		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_boss=cerebral_ceo&kv_difficulty=1&kv_raid_id=6894478&kv_hash=gkT87rGS1L",
-		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?11606-Corporate-Takedown-Fight-the-Cerebral-CEO!",
-		infoUrlTitle: "'Corporate Takedown - Fight the Cerebral CEO! ' Official Announcement",
+		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=7186882&kv_difficulty=1&kv_raid_boss=cerebral_ceo&kv_hash=1A0RG3zutj",
+//		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?11606-Corporate-Takedown-Fight-the-Cerebral-CEO!",
+//		infoUrlTitle: "'Corporate Takedown - Fight the Cerebral CEO! ' Official Announcement",
 		lootTableImageUrl: "http://i.imgur.com/7TzuHjl.jpg"
 	};
-*/
+
 	/*
 	DC_LoaTS_Helper.worldRaidInfo = {
 		name: "Cerebral Destroyer",
