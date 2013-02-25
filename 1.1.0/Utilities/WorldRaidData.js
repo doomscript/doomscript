87b5fb7a291f59d9d01190885947209a5d77e2ab
Sven <doomcat@users.noreply.github.com>
2013-02-25 03:58:40 +0000

    Removed completed RS data

--- 1.1.0/Utilities/WorldRaidData.js
+++ 1.1.0/Utilities/WorldRaidData.js
@@ -4,19 +4,19 @@
 	
 	// DC_LoaTS_Helper.worldRaidInfo.timerEnds = "2013-01-28T22:20:19Z" // 6PM EST / 11PM GMT
 
-	DC_LoaTS_Helper.worldRaidInfo = {
-		name: "Cerebral CEO",
-		
-		spawnType: "Rare Spawn",
-		
-		startDate: "02/21/2013",
-		timerEnds: "2013-02-22T22:21:04Z",
-		
-		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=7186882&kv_difficulty=1&kv_raid_boss=cerebral_ceo&kv_hash=1A0RG3zutj",
+//	DC_LoaTS_Helper.worldRaidInfo = {
+//		name: "Cerebral CEO",
+//		
+//		spawnType: "Rare Spawn",
+//		
+//		startDate: "02/21/2013",
+//		timerEnds: "2013-02-22T22:21:04Z",
+//		
+//		raidUrl: "http://www.kongregate.com/games/5thPlanetGames/legacy-of-a-thousand-suns?kv_action_type=raidhelp&kv_raid_id=7186882&kv_difficulty=1&kv_raid_boss=cerebral_ceo&kv_hash=1A0RG3zutj",
 //		infoUrl: "http://www.legacyofathousandsuns.com/forum/showthread.php?11606-Corporate-Takedown-Fight-the-Cerebral-CEO!",
 //		infoUrlTitle: "'Corporate Takedown - Fight the Cerebral CEO! ' Official Announcement",
-		lootTableImageUrl: "http://i.imgur.com/7TzuHjl.jpg"
-	};
+//		lootTableImageUrl: "http://i.imgur.com/7TzuHjl.jpg"
+//	};
 
 	/*
 	DC_LoaTS_Helper.worldRaidInfo = {
