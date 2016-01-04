6af5e6433bee53ddcb054de732566f9ea015d742
doomcat <doomcat@users.noreply.github.com>
2016-01-04 17:02:09 -0500

    Resolves #16

--- 1.2.0/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cat $DIR/header.js $DIR/Classes/*.js $DIR/MenuTabs/*.js $DIR/ChatCommands/*.js $DIR/Utilities/*.js $DIR/footer.js > $DIR/124753_nightly.user.js
-echo "124753_nightly.user.js created at $(date)";
--- 1.2.0/compileDebug.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-copy /b header.js+Classes\*.js+MenuTabs\*.js+ChatCommands\*.js+Experimental\*.js+Utilities\*.js+footer.js 124753_nightly.user.js
-echo "124753_nightly.user.js created at %date% %time%";
