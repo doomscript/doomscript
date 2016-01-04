8b3c43a1c27adfa6eabaf8b24a7317cc3736c62f
junibert <jnozaki@gmail.com>
2016-01-04 22:16:46 +0100

    Optimized fixBrokenImage's img src modification logic
    fixBrokenImage() changes the image's src attribute, doing it in place
    and modifying the value multiple times could potentially cause
    flickering and/or needless net calls.
    
    This change buffers the changes and sets the src attribute once, to the
    final value.

--- 1.1.0/Classes/RaidLink.js
+++ 1.1.0/Classes/RaidLink.js
@@ -608,50 +608,50 @@
 			// First time failed, check for alternate fail names
 			if (this.src === DC_LoaTS_Properties.lotsCDNUrl + "images/bosses/post/" + raidLink.raidTypeId + "_1.jpg" && this.src !== RaidLink.defaultImageSRC)
 			{
-				this.src = DC_LoaTS_Properties.lotsCDNUrl + "images/bosses/";
+				var src = DC_LoaTS_Properties.lotsCDNUrl + "images/bosses/";
 				switch(raidLink.raidTypeId)
 				{
 					// baseURL + "post/" + raidLink.raidTypeId + ".jpg";
 					case "celebration_enhancer_1":
 					case "weiqi_game_1":
 					case "kulnarxex_scout_ships_1":
-					case "cow_abduction_1":
+                    case "cow_abduction_1":
 					case "mutated_spacepox_1":
-                        this.src += "post/" + raidLink.raidTypeId + ".jpg";
+                        src += "post/" + raidLink.raidTypeId + ".jpg";
                         break;
 					// Raids with diviating image paths 
 					case "wr_space_pox":
-						this.src += "post/space_pox_1.jpg";
+						src += "post/space_pox_1.jpg";
 						break;
                     case "dule_warmaster_1":
                     case "dule_warmaster":
-						this.src += "post/dule_1.jpg";
+						src += "post/dule_1.jpg";
 						break;
 					case "hultex_quibberath":
-						this.src += "post/hultex_1.jpg";
+						src += "post/hultex_1.jpg";
 						break;
 					case "warden_ramiro":
-						this.src += "post/ramiro_1.jpg";
+						src += "post/ramiro_1.jpg";
 						break;
                     case "crimzo_the_killer_clown":
-                        this.src += "post/crimzo_1.jpg";
+                        src += "post/crimzo_1.jpg";
                         break;
                     case "kulnarxex_subjugator_1":                       
                     case "kulnarxex_elite_subjugator_1":
 					case "elite_kulnarxex_elite_subjugator":
-                        this.src += "post/kulnarxex_subjugator_1.jpg";
+                        src += "post/kulnarxex_subjugator_1.jpg";
                         break;
 					case "elite_birthday_cake_of_doom":
-						this.src += "post/birthday_cake_of_doom_1.jpg";
+						src += "post/birthday_cake_of_doom_1.jpg";
 						break;
 					case "elite_centurian_commander":
-						this.src += "post/commander_1.jpg";
+						src += "post/commander_1.jpg";
 						break;
 					case "elite_master_hao":
-						this.src += "post/master_hao_1.jpg";
+						src += "post/master_hao_1.jpg";
 						break;
 					case "elite_bashan":
-						this.src += "post/bashan_1.jpg";
+						src += "post/bashan_1.jpg";
 						break;
                     // These are ones that we've not found alternate images for except for the raid list image
                     case "pinatas_revenge1":
@@ -666,37 +666,37 @@
 					case "temynx_parasite1":
 					case "robot_uprising1":
 					case "besalaad_exhibit_rampage1":
-						this.src += raidLink.raidTypeId + ".$$repl$$";
-						this.src = this.src.replace("1.$$repl$$", "_small.jpg");
+						src += raidLink.raidTypeId + ".$$repl$$";
+						src = src.replace("1.$$repl$$", "_small.jpg");
 						break;
 					// Raids with image paths that don't fit known patterns
 					case "invaders_from_dimension_b":
-						this.src += "Invaders_from_Dimension_B1_small.png";
+						src += "Invaders_from_Dimension_B1_small.png";
 						break;
 					case "training_sim1":
-						this.src += "training_sim_small2.jpg";
+						src += "training_sim_small2.jpg";
 						break;
 					case "predatory_constellation":
-						this.src += "predatory_constellation_small.png"; //PNG!
+						src += "predatory_constellation_small.png"; //PNG!
 						break;
 					case "attack_of_the_gourds":
-						this.src += "Attack_of_the_Gourds_small.jpg";
+						src += "Attack_of_the_Gourds_small.jpg";
 						break;
 					case "sun_egg":
-						this.src += "the_sun_egg_small.jpg";
+						src += "the_sun_egg_small.jpg";
 						break;
 					case "hate":
-						this.src += "the_hate_walker_small.jpg";
+						src += "the_hate_walker_small.jpg";
 						break;
 					case "hukkral_war_crawler":
-						this.src += "huk-kral_war_crawler_small.jpg";
+						src += "huk-kral_war_crawler_small.jpg";
 						break;
 					case "elite_titanomachy":
-						this.src += "z27_boss_small.jpg"; //omg what 5pg?
+						src += "z27_boss_small.jpg"; //omg what 5pg?
 						break;
 					case "giant_kwelshax":
 					case "elite_giant_kwelshax":
-						this.src += "giant_kwelshax_small.jpg";
+						src += "giant_kwelshax_small.jpg";
 						break;
                     case "purple_lion":
 					case "kang":
@@ -722,8 +722,9 @@
                     case "ship_pinata":
                     case "dimetrodon_riot":
 					default:
-                        this.src += raidLink.raidTypeId + "_small.jpg";
+                        src += raidLink.raidTypeId + "_small.jpg";
 				}
+				this.src = src;
 			}
 			// Second time failed, switch to default
 			else if (this.src != RaidLink.defaultImageSRC)
